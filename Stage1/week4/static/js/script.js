82ee0345b86a98505213073be5eaf7eedef7e637
54Laichu <machi123j@gmail.com>
2024-04-23 21:42:09 +0800

    chore: cleanup unnecessary code

--- Stage1/week4/static/js/script.js
+++ Stage1/week4/static/js/script.js
@@ -1,10 +1,12 @@
-window.onload = function() {
+window.onload = function () {
   let form = document.querySelector("#signinForm");
-  form.onsubmit = function(event) {
+
+  form.addEventListener('submit', function (event) {
     let checkbox = document.querySelector("#checkbox");
+
     if (!checkbox.checked) {
-      event.preventDefault();
-      alert("Please check the checkbox first");
+      window.alert("Please check the checkbox first");
+      event.preventDefault()
     }
-  };
-};
+  });
+}
